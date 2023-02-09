a5f57f858e2219ee1195596a0154bf9954b07dff
kukgini <kukgini@gmail.com>
2023-02-09 16:54:35 +0900

    enhance rustywest example

--- rustywest/src/main.rs
+++ rustywest/src/main.rs
@@ -1,20 +1,23 @@
 use reqwest;
 use std::error::Error;
 use std::io;
-use std::process;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
-    let api_key = "api-key";
-    let api_token = "api-token";
+    let api_key : String = env::var("RUSTYWEST_API_KEY").unwrap();
+    let api_token : String = env::var("RUSTYWEST_API_TOKEN").unwrap();
+    let url_prefix: String = env::var("RUSTYWEST_API_URL").unwrap();
+
     let client = reqwest::Client::new();
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
         let record = result?;
         let id = record.get(0).unwrap();
-        let url = format!("https://some.url/{}",id);
+        let mut url = url_prefix.clone();
+        url.push_str(id);
         let result = client.delete(&url)
-            .header("X-API-KEY", api_key)
+            .header("X-API-KEY", api_key.to_owned())
             .header("Authorization", format!("Bearer {}", api_token))
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
