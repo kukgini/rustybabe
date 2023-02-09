a7ec8df205c85486e4761a1ef2beca9a8bf86abb
kukgini <kukgini@gmail.com>
2023-02-09 17:14:43 +0900

    enhance serde json example.

--- rustyserde/src/parser.rs
+++ rustyserde/src/parser.rs
@@ -6,9 +6,9 @@ pub fn untyped_example() -> Result<()> {
     let json = serde_json::from_reader(io::stdin());
     let v: Value = json.unwrap();
     if let Some(results) = v["results"].as_array() {
-        println!("credential_exchange_id");
+        println!("id");
         for result in results {
-            println!("{}", result["credential_exchange_id"]);
+            println!("{}", result["id"].as_str().unwrap());
         }
     }
     Ok(())
